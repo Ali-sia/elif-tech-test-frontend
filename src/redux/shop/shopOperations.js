6b2281a1ca5fa1ba83db149efe65eef631ed2f24
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-25 10:32:32 +0300

    update: shop operations
    create operation to fetch shop list

--- src/redux/shop/shopOperations.js
+++ src/redux/shop/shopOperations.js
@@ -1,6 +1,16 @@
-import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// axios.defaults.baseURL = '';
+import { privateApi } from 'http/http';
 
-
+// GET @ /shops
+export const fetchShops = createAsyncThunk(
+  'shops/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await privateApi.get('/shops');
+      return response.data.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
