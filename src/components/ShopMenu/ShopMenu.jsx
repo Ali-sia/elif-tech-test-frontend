3f34ed9dfc1aaeb3db6c6429adb4bdf36a16b638
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-25 20:13:29 +0300

    Update ShopMenu.jsx

--- src/components/ShopMenu/ShopMenu.jsx
+++ src/components/ShopMenu/ShopMenu.jsx
@@ -1,13 +1,19 @@
-import { useEffect } from 'react';
+import {
+  useEffect,
+  // , useState
+} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { getShopMenu } from '../../redux/menu/menuSelectors';
 import { fetchShopMenu } from 'redux/menu/menuOperations';
 
 import { addToCart } from '../../redux/cart/cartSlice';
+import { getCart } from '../../redux/cart/cartSelectors';
 
 const ShopMenu = ({ shopId }) => {
   const dispatch = useDispatch();
+  const cart = useSelector(getCart);
+  // const [isOneShop, setIsOneShop] = useState(true);
 
   useEffect(() => {
     dispatch(fetchShopMenu(shopId));
@@ -18,30 +24,39 @@ const ShopMenu = ({ shopId }) => {
     return null;
   }
 
-  const handleAddToCart = itemId => {
-    dispatch(addToCart(itemId));
+  const handleAddToCart = item => {
+    const isAnotherShop = cart.find(test => test.owner !== item.owner);
+
+    if (isAnotherShop) {
+      // setIsOneShop(false);
+      return null;
+    }
+
+    dispatch(addToCart(item));
   };
 
   return (
-    <ul>
-      {menu.map(item => {
-        return (
-          <li key={item._id}>
-            <div>
-              {item.photo && <img src={item.photo} alt="" />}
-              photo
-            </div>
-            <div>
-              <h3>{item.name}</h3>
-              <p>Price: {item.price}$</p>
-              <button type="button" onClick={() => handleAddToCart(item)}>
-                BUY
-              </button>
-            </div>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <ul>
+        {menu.map((item, index) => {
+          return (
+            <li key={`${item._id}-${index}`}>
+              <div>
+                {item.photo && <img src={item.photo} alt="" />}
+                photo
+              </div>
+              <div>
+                <h3>{item.name}</h3>
+                <p>Price: {item.price}$</p>
+                <button type="button" onClick={() => handleAddToCart(item)}>
+                  BUY
+                </button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
 
