9c84ca557a318aa7b3fd9929aa49f1bf35d80be5
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-25 14:43:53 +0300

    delete comments

--- src/redux/menu/menuOperations.js
+++ src/redux/menu/menuOperations.js
@@ -8,7 +8,6 @@ export const fetchShopMenu = createAsyncThunk(
   async (shopId, thunkAPI) => {
     try {
       const response = await privateApi.get(`/menus/${shopId}`);
-      console.log('---------------------', response.data.data);
       return response.data.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
