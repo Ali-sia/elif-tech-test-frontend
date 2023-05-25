b6dd276b67f47aa4f1e838c9db77979cb2f4dc05
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-25 11:00:37 +0300

    update: shop slice
    create fetching shop list

--- src/redux/shop/shopSlice.js
+++ src/redux/shop/shopSlice.js
@@ -1,15 +1,31 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const shopInitialState = {};
+import { fetchShops } from './shopOperations';
+
+const shopInitialState = {
+  items: [],
+  isLoading: false,
+  error: null,
+};
+
 export const shopSlice = createSlice({
   name: 'shops',
   initialState: shopInitialState,
-  reducers: {
-    // setShop: (_, action) => {
-    //   return action.payload;
-    // },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchShops.fulfilled, (state, { payload }) => {
+        state.items = payload;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(fetchShops.pending, (state, { payload }) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchShops.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = payload;
+      });
   },
 });
-
 
 export const shopReducer = shopSlice.reducer;
