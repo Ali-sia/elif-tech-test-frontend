0e975fa8e10807f4bd91e149e94bbacf83dd6dea
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-25 23:34:45 +0300

    update: set default shop

--- src/components/ShopList/ShopList.jsx
+++ src/components/ShopList/ShopList.jsx
@@ -1,18 +1,26 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { getShopsList } from '../../redux/shop/shopSelectors';
 import { fetchShops } from 'redux/shop/shopOperations';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const ShopList = ({ setVariant }) => {
   const dispatch = useDispatch();
+  const shops = useSelector(getShopsList);
+  const [selectedShop, setSelectedShop] = useState(null);
 
   useEffect(() => {
     dispatch(fetchShops());
   }, [dispatch]);
 
-  const shops = useSelector(getShopsList);
+  useEffect(() => {
+    if (shops && shops.length > 0) {
+      setSelectedShop(shops[0]._id);
+      setVariant(shops[0]._id);
+    }
+  }, [shops, setVariant]);
 
   const handleVariantChange = event => {
+    setSelectedShop(event.target.value);
     setVariant(event.target.value);
   };
 
@@ -29,6 +37,7 @@ const ShopList = ({ setVariant }) => {
                 type="radio"
                 name="shop"
                 value={`${shop._id}`}
+                checked={selectedShop === shop._id}
                 onClick={handleVariantChange}
               />
               {shop.name}
