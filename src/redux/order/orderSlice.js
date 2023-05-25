f0683359cbf45d8585632ebd21d4ecec86d6e516
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-25 22:55:05 +0300

    Update orderSlice.js

--- src/redux/order/orderSlice.js
+++ src/redux/order/orderSlice.js
@@ -1,15 +1,30 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const orderInitialState = {};
+import { addOrder } from './orderOperations';
+
+const orderInitialState = {
+  items: [],
+  isLoading: false,
+  error: null,
+};
 export const orderSlice = createSlice({
   name: 'orders',
   initialState: orderInitialState,
-  reducers: {
-    // setShop: (_, action) => {
-    //   return action.payload;
-    // },
+  extraReducers: builder => {
+    builder
+      .addCase(addOrder.fulfilled, (state, { payload }) => {
+        state.items = payload;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(addOrder.pending, (state, { payload }) => {
+        state.isLoading = true;
+      })
+      .addCase(addOrder.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = payload;
+      });
   },
 });
-
 
 export const orderReducer = orderSlice.reducer;
