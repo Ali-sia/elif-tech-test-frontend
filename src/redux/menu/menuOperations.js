03308cfdefb89379663465c037ee676a29e529f6
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-25 12:18:35 +0300

    update: menu operations - add fetch shop menu by shop id

--- src/redux/menu/menuOperations.js
+++ src/redux/menu/menuOperations.js
@@ -1,6 +1,17 @@
-import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// axios.defaults.baseURL = '';
+import { privateApi } from 'http/http';
 
-
+// GET @ /shops
+export const fetchShopMenu = createAsyncThunk(
+  'menus/fetchShopMenu',
+  async (shopId, thunkAPI) => {
+    try {
+      const response = await privateApi.get(`/menus/${shopId}`);
+      console.log('---------------------', response.data.data);
+      return response.data.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
