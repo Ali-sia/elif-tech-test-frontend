cc493ad7ca532aaa04db71717b0f5dfb8b0b9a34
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-26 00:03:20 +0300

    Update http.jsx

--- src/http/http.jsx
+++ src/http/http.jsx
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 export const publicApi = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  // baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://elif-tech-test-api.onrender.com/api',
 });
 
 export const privateApi = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  // baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://elif-tech-test-api.onrender.com/api',
 });
