54887fca152b21ed4706f8560a4892229ce62785
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-25 12:17:29 +0300

    update: menu slice - add reduser

--- src/redux/menu/menuSlice.js
+++ src/redux/menu/menuSlice.js
@@ -1,15 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchShopMenu } from './menuOperations';
 
-const menuInitialState = {};
+const menuInitialState = {
+  items: [],
+  isLoading: false,
+  error: null,
+};
 export const menuSlice = createSlice({
   name: 'menus',
   initialState: menuInitialState,
-  reducers: {
-    // setShop: (_, action) => {
-    //   return action.payload;
-    // },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchShopMenu.fulfilled, (state, { payload }) => {
+        state.items = payload;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(fetchShopMenu.pending, (state, { payload }) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchShopMenu.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = payload;
+      });
   },
 });
-
 
 export const menuReducer = menuSlice.reducer;
