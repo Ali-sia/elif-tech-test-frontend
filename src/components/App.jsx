00bd527d3895c713a8a843d89b62af9d84ce99a1
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-25 10:00:37 +0300

    add: routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,25 @@
-// import PropTypes from 'prop-types';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchContacts } from '../redux/operations';
-import { getError, getIsLoading } from '../redux/selectors';
+import Layout from './Layout/Layout';
 
-import { GlobalStyle } from './GlobalStyle';
-import { Box } from './Box';
-
-import ContactList from './ContactList';
-import Filter from './Filter';
-import ContactForm from './ContactForm';
-import Title from './Title/index';
+const ShopPage = lazy(() => import('../page/ShopPage/ShopPage'));
+const CartPage = lazy(() => import('../page/CartPage/CartPage'));
+const NotFoundPage = lazy(() => import('../page/NotFoundPage/NotFoundPage'));
 
 export const App = () => {
-  const dispatch = useDispatch();
-  const isLoading = useSelector(getIsLoading);
-  const error = useSelector(getError);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-
   return (
-    <Box pr={4} pl={4} color="text" width="400px">
-      <Title children="Add contact" />
-      <ContactForm />
+    <BrowserRouter basename="/elif-tech-test-frontend">
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<ShopPage />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Route>
 
-      <Title children="Contacts" />
-      <Filter />
-      {isLoading && !error && <b>Request in progress...</b>}
-      {!isLoading && !error && <b>DONE</b>}
-      <ContactList />
-
-      <GlobalStyle />
-    </Box>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 };
-
-// App.propTypes = {
-//   contacts: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.string.isRequired,
-//       name: PropTypes.string.isRequired,
-//       number: PropTypes.string.isRequired,
-//     })
-//   ),
-//   filter: PropTypes.string,
-// };
