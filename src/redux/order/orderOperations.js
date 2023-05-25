58403c4c28c996c2cd446f5ee4a24bb4df6a24ca
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-25 22:55:09 +0300

    Update orderOperations.js

--- src/redux/order/orderOperations.js
+++ src/redux/order/orderOperations.js
@@ -1,6 +1,16 @@
-import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// axios.defaults.baseURL = '';
+import { privateApi } from 'http/http';
 
-
+// GET @ /shops
+export const addOrder = createAsyncThunk(
+  'orders/addOrder',
+  async (newOrder, thunkAPI) => {
+    try {
+      const response = await privateApi.post(`/orders`, { ...newOrder });
+      return response.data.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
