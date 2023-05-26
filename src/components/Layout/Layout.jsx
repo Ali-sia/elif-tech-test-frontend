736ccdf3c9e3e8bdc4cae8a4d82ebb11b7c393e4
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-26 10:21:08 +0300

    create Layout styles

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,19 +1,20 @@
 import { Suspense } from 'react';
-import { Outlet, NavLink } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import { Toaster } from 'react-hot-toast';
 
+import { SyledNavLink, Header } from './Layout.styled';
 import { GlobalStyle } from '../GlobalStyle';
 import { Box } from '../Box';
 
 const Layout = () => {
   return (
     <>
-      <header>
-        <NavLink to="/">Shop</NavLink>
-        <NavLink to="/cart">Cart</NavLink>
-      </header>
-      <Box pr={4} pl={4} color="text" width="400px">
+      <Header>
+        <SyledNavLink to="/">Shop</SyledNavLink>
+        <SyledNavLink to="/cart">Cart</SyledNavLink>
+      </Header>
+      <Box padding={4} color="text" width="400px">
         <Suspense fallback={null}>
           <Outlet />
         </Suspense>
---
+++ src/components/Layout/Layout.styled.jsx
@@ -0,0 +1,28 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const SyledNavLink = styled(NavLink)`
+  color: #000000;
+
+  padding: ${p => p.theme.space[3]}px;
+  border-radius: ${p => p.theme.radii.sm};
+  text-decoration: none;
+
+  &.active {
+    background-color: ${p => p.theme.colors.primary};
+    color: ${p => p.theme.colors.white};
+  }
+
+  :hover:not(.active),
+  :focus-visible:not(.active) {
+    color: ${p => p.theme.colors.primary};
+  }
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  height: ${p => p.theme.space[6] - 4}px;
+  padding: ${p => p.theme.space[5] - 6}px;
+  background-color: ${p => p.theme.colors.secondary};
+`;
